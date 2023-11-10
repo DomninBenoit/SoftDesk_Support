ec8153e193fdea6137f4907f65b711865626712c
Domnin <dombouif@gmail.com>
2023-11-10 14:38:41 +0100

    add pagination

--- SoftDesk/projects/views.py
+++ SoftDesk/projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework import permissions, status
@@ -31,9 +31,18 @@ class ContributorsViewSet(ViewSet):
             return Response({'status': 'User added as contributor'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request, project_id=None,):
+    def get(self, request, project_id=None):
         project = self.get_project(project_id)
         contributors = project.contributors.all()
+
+        # Appliquer la pagination
+        paginator = PageNumberPagination()
+        page = paginator.paginate_queryset(contributors, request)
+        if page is not None:
+            serializer = UserSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+
+        # Si pas de pagination nécessaire
         serializer = UserSerializer(contributors, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
